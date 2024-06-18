1e880e56b39708dc7a849eeb101b93e7510f5bc5
“Natan <natanfoleto2015@hotmail.com>
2024-06-18 14:53:45 -0300

    Added opengraph-image and cart component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,3 +1,4 @@
+import { CartProvider } from '@/contexts/cart-context'
 import { Header } from '../../components/header'
 import type { ReactNode } from 'react'
 
@@ -7,9 +8,11 @@ interface StoreLayoutProps {
 
 export default function StoreLayout({ children }: StoreLayoutProps) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
